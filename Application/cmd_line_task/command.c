e67ad9763a3c850dcfa69d4d654120b52174cffa
HalmondD <duonganhkhoa2002@gmail.com>
2025-06-17 11:17:19 +0700

    change hv pulse min from 1ms to 5ms, lv pulse max from 1000ms to 90ms

--- Application/cmd_line_task/command.c
+++ Application/cmd_line_task/command.c
@@ -660,9 +660,9 @@ int CMD_SET_PULSE_HV_POS(int argc, char *argv[])
 	receive_argm[0] = atoi(argv[1]);
 	receive_argm[1] = atoi(argv[2]);
 
-	if ((receive_argm[0] > 20) || (receive_argm[0] < 1))
+	if ((receive_argm[0] > 20) || (receive_argm[0] < 5))
 		return CMDLINE_INVALID_ARG;
-	else if ((receive_argm[1] > 20) || (receive_argm[1] < 1))
+	else if ((receive_argm[1] > 20) || (receive_argm[1] < 5))
 		return CMDLINE_INVALID_ARG;
 
 	if ((HB_sequence_array[CMD_sequence_index].is_setted & (1 << 4)) == false)
@@ -697,9 +697,9 @@ int CMD_SET_PULSE_HV_NEG(int argc, char *argv[])
 	receive_argm[0] = atoi(argv[1]);
 	receive_argm[1] = atoi(argv[2]);
 
-	if ((receive_argm[0] > 20) || (receive_argm[0] < 1))
+	if ((receive_argm[0] > 20) || (receive_argm[0] < 5))
 		return CMDLINE_INVALID_ARG;
-	else if ((receive_argm[1] > 20) || (receive_argm[1] < 1))
+	else if ((receive_argm[1] > 20) || (receive_argm[1] < 5))
 		return CMDLINE_INVALID_ARG;
 
 	if ((HB_sequence_array[CMD_sequence_index].is_setted & (1 << 4)) == false)
@@ -734,9 +734,9 @@ int CMD_SET_PULSE_LV_POS(int argc, char *argv[])
 	receive_argm[0] = atoi(argv[1]);
 	receive_argm[1] = atoi(argv[2]);
 
-	if ((receive_argm[0] > 1000) || (receive_argm[0] < 1))
+	if ((receive_argm[0] > 90) || (receive_argm[0] < 1))
 		return CMDLINE_INVALID_ARG;
-	else if ((receive_argm[1] > 1000) || (receive_argm[1] < 1))
+	else if ((receive_argm[1] > 90) || (receive_argm[1] < 1))
 		return CMDLINE_INVALID_ARG;
 
 	if ((HB_sequence_array[CMD_sequence_index].is_setted & (1 << 5)) == false)
@@ -774,9 +774,9 @@ int CMD_SET_PULSE_LV_NEG(int argc, char *argv[])
 	receive_argm[0] = atoi(argv[1]);
 	receive_argm[1] = atoi(argv[2]);
 
-	if ((receive_argm[0] > 1000) || (receive_argm[0] < 1))
+	if ((receive_argm[0] > 90) || (receive_argm[0] < 1))
 		return CMDLINE_INVALID_ARG;
-	else if ((receive_argm[1] > 1000) || (receive_argm[1] < 1))
+	else if ((receive_argm[1] > 90) || (receive_argm[1] < 1))
 		return CMDLINE_INVALID_ARG;
 
 	if ((HB_sequence_array[CMD_sequence_index].is_setted & (1 << 5)) == false)
